346c6ea41f8157d24ddc9d9d7ac8dd15523cd6de
louispaulet <louispaulet13@gmail.com>
2023-05-17 00:50:47 +0200

    make a sharing button with custom animation

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ var select = document.getElementById('jsonFileSelect');
 jsonFiles.forEach(filename => {
   var option = document.createElement('option');
   option.text = filename;
-  option.value = `json_files/${filename}`;
+  option.value = `${filename}`;
   select.add(option);
 });
 
@@ -54,16 +54,55 @@ function createTimeline(jsonFilePath) {
 
 // Get JSON file from URL parameters or default to the first JSON file
 var params = new URLSearchParams(window.location.search);
-var jsonFile = params.get('jsonFile') || `json_files/${jsonFiles[0]}`;
-
-// Set the selected option in the dropdown
-select.value = jsonFile;
+var jsonFile = params.get('jsonFile') || jsonFiles[0];
 
 // Create the initial timeline from the JSON file
-createTimeline(jsonFile);
+createTimeline(`json_files/${jsonFile}`);
+
+// Update the URL for the initial timeline
+window.history.replaceState({}, '', `?jsonFile=${jsonFile}`);
+
+// Set the selected option in the dropdown after the dropdown has been populated
+window.onload = function() {
+  select.value = `${jsonFile}`;
+}
+
 
 // Update the timeline and the URL when a new JSON file is selected
 select.addEventListener('change', function() {
-  createTimeline(this.value);
+  createTimeline(`json_files/${this.value}`);
   window.history.pushState({}, '', `?jsonFile=${this.value}`);
+});
+
+// Add event listener for the "Share" button
+document.getElementById('share-button').addEventListener('click', function() {
+  var button = this; // Get a reference to the button
+
+  // Store the current label and color
+  var previousLabel = button.innerText;
+  var previousColor = button.classList.contains('btn-primary') ? 'btn-primary' : 'btn-secondary';
+
+  // Update the button label and color
+  button.innerText = 'Link Copied!';
+  button.classList.remove(previousColor);
+  button.classList.add('btn-success');
+
+  // Copy URL to clipboard
+  navigator.clipboard.writeText(window.location.href)
+    .then(function() {
+      // Revert the label and color after a delay
+      setTimeout(function() {
+        button.innerText = previousLabel;
+        button.classList.remove('btn-success');
+        button.classList.add(previousColor);
+      }, 3000); // Change the delay time (in milliseconds) as needed
+    })
+    .catch(function(err) {
+      console.error('Could not copy text: ', err);
+      // Handle error if clipboard write fails
+      // Revert the label and color immediately
+      button.innerText = previousLabel;
+      button.classList.remove('btn-success');
+      button.classList.add(previousColor);
+    });
 });
